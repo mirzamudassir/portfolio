74f9518cbc742025e28dfb22a810f52d0ac407c1
Mudassir Mirza <mudassir@debian-BULLSEYE-live-builder-AMD64>
2022-12-30 01:49:08 +0500

    updated configs

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+    import LandingPage from "./landingPage.svelte";
+
+
+</script>
+
+<main>
+<LandingPage/>
+</main>
+
+<style>
+
+</style>
---
+++ src/landingPage.svelte
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<main>
+<h1>Landing Page</h1>
+</main>
+
+<style>
+
+</style>
